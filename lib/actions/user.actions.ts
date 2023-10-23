fb58cd64f3332def577070eca3ec039e0076ae30
LebyakMarki <lebyak.m@gmail.com>
2023-10-23 17:42:36 +0200

    Add profile page

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,6 +3,7 @@
 import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose"
+import Thread from "../models/thread.model";
 
 interface Params {
     userId: string,
@@ -52,3 +53,28 @@ export async function fetchUser(userId: string) {
         throw new Error(`Failed to fetch user: ${error}`);
     }
 }
+
+export async function fetchUserPosts(userId: string) {
+    try {
+      connectToDB();
+      const threads = await User.findOne({ id: userId }).populate({
+        path: "threads",
+        model: Thread,
+        populate: [
+          {
+            path: "children",
+            model: Thread,
+            populate: {
+              path: "author",
+              model: User,
+              select: "name image id",
+            },
+          },
+        ],
+      });
+      return threads;
+    } catch (error) {
+      console.error("Error fetching user threads:", error);
+      throw error;
+    }
+  }
